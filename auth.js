5d495838a7a1ea5fa7429a8f440b2d7d868a4827
Rohit <rohityadav2604@gmail.com>
2021-09-20 18:18:01 +0530

    auth

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const user = require("./model/user");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
-
+const saltround = 10;
 dotenv.config();
 
 const app = express();
@@ -20,12 +20,26 @@ let checkuser = async(req , res , next) => {
         let obj = JSON.parse(req.query.body);
        let alluser = await user.find();
        let f =0;
-       alluser.forEach((user)=>{
+       console.log(obj);
+       
+       alluser.forEach(async(user)=>{
+        
            if(obj.email == user.email)
            {
-               f=1;
-               res.send("user already exist");
-           }
+               console.log("inside if block");
+                  let pass = bcrypt.compareSync(obj.password , user.password);
+                  if(pass)
+                  {
+                      res.send("user exist");
+                  }
+                  else
+                  {
+                      res.send("password is incorrect");
+                  }
+                  
+                  
+            }   
+           
        })
        if(f==0)
        next();
@@ -39,13 +53,12 @@ let checkuser = async(req , res , next) => {
 
 async function hash(password)
 {
-    let saltround = Math.floor(Math.random()*20)+1;
-    console.log(saltround);
+   // let saltround = Math.floor(Math.random()*20)+1;
     let hashpassword = await bcrypt.hash(password , saltround);
     return {hashpassword , saltround};
 }
 
-app.get('/user/signup', checkuser , async(req , res)=>{
+app.get('/user/signup/auth', checkuser , async(req , res)=>{
     try{
 
         let obj = JSON.parse(req.query.body);
@@ -65,6 +78,10 @@ app.get('/user/signup', checkuser , async(req , res)=>{
     {
         console.log(err);
     }
+})
+
+app.get("/user/login/auth" , checkuser , async(req , res)=>{
+     res.send("signup first");
 })
 
 
